3b3fc9efa253224f5876756c5dcb474c00e4bc87
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2022-10-15 13:02:28 -0300

    commit

--- exercicios/ex26/mq005/script/script.js
+++ exercicios/ex26/mq005/script/script.js
@@ -1,6 +1,6 @@
 function onclickMenu() {
     if (itensMenu.style.display == 'block') {
-        itensMenu.style.display = 'none'
+        itensMenu.style.display = 'none' // id > estilo > display
     } else {
         itensMenu.style.display = 'block'
     }
