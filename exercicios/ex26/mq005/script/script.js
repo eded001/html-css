87c9e196e8274efa85deee409b8a02e52e886013
Edgar Augusto <108343328+eded001@users.noreply.github.com>
2022-11-06 19:00:36 -0200

    commit

--- exercicios/ex26/mq005/script/script.js
+++ exercicios/ex26/mq005/script/script.js
@@ -1,7 +1,15 @@
-function onclickMenu() {
+function cliqueMenu() {
     if (itensMenu.style.display == 'block') {
         itensMenu.style.display = 'none' // id > estilo > display
     } else {
         itensMenu.style.display = 'block'
+    }
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        itensMenu.style.display = 'block'
+    } else {
+        itensMenu.style.display = 'none'
     }
 }
